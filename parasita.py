337a4ecf314d47ceaa5d109a841d0da27b2ab9b7
Pedro Otávio <pedr_ofs@hotmail.com>
2022-03-01 10:25:38 -0500

    Update parasita.py

--- parasita.py
+++ parasita.py
@@ -7,7 +7,7 @@
 # Este script tem por finalidade realizar o ataques ARP Spoofing em uma rede.
 
 # Este script possui a capacidade de executar ataques "Man in the Middle" através do envenanamento da
-# tabela ARP de um determinado host da rede. Possui também a função "DOS". Podendo ser utilizada para
+# tabela ARP de determinados hosts da rede. Possui também a função "DOS". Podendo ser utilizada para
 # realiza ataque de negação de serviço em um único host da rede.
 
 # As bibliotecas "Scapy" e "Getmac" serão necessárias para a correta execução deste script. Sendo assim,
